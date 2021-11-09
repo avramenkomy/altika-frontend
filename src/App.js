312fa0e417ba3a294747ba46678d46d561a16a7b
avramenkomy <avramenkomy@yandex.ru>
2021-11-09 16:55:58 +0300

    WIP: create custom theme for project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { Paper, Container } from '@material-ui/core';
 
 import AppHeader from './components/appHeader/AppHeader';
-import AppContent from './components/appContent/AppContent';
+// import Logo from './components/Logo';
+
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        light: '#508BB6',
+        main: '#256EA4',
+        dark: '#006EAA',
+        contrastText: '#F0F0F1'
+      },
+      secondary: {
+        light: '#FFF',
+        main: '#E0E1E2',
+        dark: '#F0F0F1',
+        contrastText: '#000'
+      },
+    }
+  });
+  console.log(theme)
+
   return (
-    <div className="app">
-      <AppHeader />
-      <AppContent />
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <AppHeader darkTheme={darkMode} onChangeTheme={setDarkMode} />
+      <Container
+        style={{ marginTop: '1rem'}}
+        disableGutters
+      >
+        <Paper style={{ width: '100%', height: '100vh' }} square />
+      </Container>
+
+    </ThemeProvider>
+  );
 }
 
 export default App;
