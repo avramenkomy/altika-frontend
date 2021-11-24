c148a3677b0088ee436e0a9805ed41fd8efde7c6
avramenkomy <avramenkomy@yandex.ru>
2021-11-24 19:29:02 +0300

    WIP: add card

--- src/components/expertiseTypes/ExpertiseCard.js
+++ src/components/expertiseTypes/ExpertiseCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Card, CardActions, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 300,
+    maxWidth: 400,
+    minWidth: 250,
     margin: theme.spacing(2),
   },
   media: {
     height: 200,
-
+  },
+  card: {
+    textAlign: 'center',
   },
 }));
 
@@ -18,21 +21,19 @@ function ExpertiseCard(props) {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.url}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image={props.url}
+        title={props.title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.description}
+        </Typography>
+      </CardContent>
     </Card>
   )
 }
