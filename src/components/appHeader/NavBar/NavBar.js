49314984abb807e73efa1b2c308c8099896532cf
avramenkomy <mihail.avramenko@ocrv.ru>
2021-11-27 14:04:02 +0300

    WIP: change styles

--- src/components/appHeader/NavBar/NavBar.js
+++ src/components/appHeader/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import { Toolbar, Link } from '@material-ui/core';
 import { Grid } from '@mui/material';
 import { makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,9 +22,14 @@ export default function NavBar() {
   const preventDefault = (event) => event.preventDefault();
   const widthMore800 = useMediaQuery('(min-width:800px)');
 
+  const rootStyle = clsx({
+    [classes.root] : true,
+    [classes.textAlignCenter] : widthMore800
+  });
+
   return (
     <Toolbar disableGutters className={classes.root}>
-      <Grid container className={classes.root && (widthMore800 && classes.textAlignCenter)} alignContent="center" alignItems="center">
+      <Grid container className={rootStyle} alignContent="center" alignItems="center">
         <Grid item xs={12} md={3}>
             <Link href="#" className={classes.link} variant="h6" onClick={preventDefault} color="secondary" underline="none">
               О нас
